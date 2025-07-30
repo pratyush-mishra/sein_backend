2f15815877dc1a4b9720a0d0fbfc92c7e4d1f423
Pratyush Mishra <pratyushmishra0211@gmail.com>
2025-07-30 16:19:32 +0100

    remove admin details

--- resource_bank/build.sh
+++ resource_bank/build.sh
@@ -7,17 +7,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py shell << EOF
-from users.models import Member
-import os
-admin_email = os.environ.get('ADMIN_EMAIL', 'info@seinglasgow.org.uk')
-admin_username = os.environ.get('ADMIN_USERNAME', 'info@seinglasgow.org.uk')
-admin_password = os.environ.get('ADMIN_PASSWORD', 'IvanSuliJani2022!')
-if not Member.objects.filter(username=admin_username).exists():
-    Member.objects.create_superuser(
-        username=admin_username,
-        email=admin_email,
-        password=admin_password
-    )
-    print('Superuser created')
-EOF
+
