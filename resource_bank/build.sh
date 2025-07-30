7f06c6490ca10856560acc1e8fec1061219ac2ce
Pratyush Mishra <pratyushmishra0211@gmail.com>
2025-07-30 15:47:25 +0100

    add admin creds

--- resource_bank/build.sh
+++ resource_bank/build.sh
@@ -6,3 +6,15 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+# Create superuser if it doesn't exist
+python manage.py shell << EOF
+from django.contrib.auth.models import User
+import os
+if not User.objects.filter(username=os.environ.get('ADMIN_USERNAME', 'admin')).exists():
+    User.objects.create_superuser(
+        username=os.environ.get('ADMIN_USERNAME', 'admin'),
+        email=os.environ.get('ADMIN_EMAIL', 'info@seinglasgow.org.uk'),
+        password=os.environ.get('ADMIN_PASSWORD', 'IvanSuliJani2022!')
+    )
+    print('Superuser created')
+EOF
